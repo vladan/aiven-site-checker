1e6dc29c34e61eb9de733a94d5f1cfe0a550f779
Vladan Popovic <vladanovic@gmail.com>
2020-09-07 14:52:33 +0200

    Add __version__ to __init__.py

---
+++ src/chweb/__init__.py
@@ -0,0 +1,10 @@
+"""
+Package related information, like version, name, etc.
+"""
+from pkg_resources import get_distribution, DistributionNotFound
+
+try:
+    __version__ = get_distribution(__name__).version
+except DistributionNotFound:
+    # package is not installed
+    pass
